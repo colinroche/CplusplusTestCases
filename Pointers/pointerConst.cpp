31538a89c8bbbeeb505b062daa4399d0a0402c60
colinroche <colinproche@gmail.com>
2023-10-30 12:39:16 +0000

    Pointers and constants

---
+++ Pointers/pointerConst.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#define END "\n\n"
+#define LINESPACE '\n'
+
+using namespace std;
+
+void increment_all(int* start, int* stop)
+{
+    int * current = start;
+
+    cout << *start << "\n\n";
+
+    while (current != stop)
+    {
+        ++(*current);
+        ++current;
+    }
+}
+
+void print_all(const int* start, const int* stop)
+{
+    const int * current = start;
+
+    while (current != stop)
+    {
+        cout << *current << ", ";
+        ++current;
+    }
+}
+
+int main () {
+    int numbers[]{10, 20, 30, 40, 50, 60};
+
+    increment_all(numbers, numbers + 6);
+    print_all(numbers, numbers+6);
+    cout << END;
+    return 0;
+}
