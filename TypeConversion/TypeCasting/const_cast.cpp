17b85218b899023812beda0c762742b9ebea567b
colinroche <colinproche@gmail.com>
2023-11-01 22:34:33 +0000

    const_cast

---
+++ TypeConversion/TypeCasting/const_cast.cpp
@@ -0,0 +1,16 @@
+// pass a const pointer to a function that expects a non-const argument
+
+// const_cast
+#include <iostream>
+using namespace std;
+
+void print (char * str)
+{
+  cout << str << '\n';
+}
+
+int main () {
+  const char * c = "sample text";
+  print ( const_cast<char *> (c) );
+  return 0;
+}
