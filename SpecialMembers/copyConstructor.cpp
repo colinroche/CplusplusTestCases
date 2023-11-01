d766bcbef9c722529b6bd972311d2efdcbbc8f0d
colinroche <colinproche@gmail.com>
2023-11-01 14:37:10 +0000

    Copy constructor

--- SpecialMembers/copyConstructor.cpp
+++ SpecialMembers/copyConstructor.cpp
@@ -1,26 +1,29 @@
+// copy constructor - deep copy - needed when using pointer, etc.
+
 #include <iostream>
 #include <string>
 using namespace std;
 
-// using Example4 from defaultCosntructor.cpp
+// using Example4 from deconstructor.cpp
 class Example4 {
         string* ptr;
     public:
         // constructor
-        Example4() : ptr(new string) {}
         Example4(const string& str) : ptr(new string(str)) {}
         // deconstructor
         ~Example4() { delete ptr; }
+        // copy constructor
+        Example4(const Example4& x) : ptr(new string(x.content())) {}
         // access content
         const string& content() const { return *ptr; }
 };
 
 int main() {
-    Example4 ex4_1; // empty block as no contetn has been set
     Example4 ex4("value");
+    Example4 ex5(ex4);
 
-    cout << "Example4_1 content: " << ex4_1.content() << '\n';
     cout << "Example4 content: " << ex4.content() << '\n';
+    cout << "Example4 copy content: " << ex5.content() << '\n';
 
     return 0;
 }
